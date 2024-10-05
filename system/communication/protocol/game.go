04c11a8d08f273a863afce94631fef4f579f7c5e
LuchoLorenzo1 <LuchoLorenzo1@users.noreply.github.com>
2024-10-05 16:18:03 -0300

    adding review and game structs

---
+++ system/communication/protocol/game.go
@@ -0,0 +1,132 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strconv"
+) 
+
+type Game struct {
+	AppID                  string
+	Name                   string
+	WindowsCompatible      bool
+	MacCompatible          bool
+	LinuxCompatible        bool
+	Genres                 string
+	AveragePlaytimeForever int
+	ReleaseDate            string
+}
+
+func gameFromRecord(record []string) (Game, error) {
+	game := Game{
+		AppID:       record[0],
+		Name:        record[1],
+		ReleaseDate: record[2],
+		Genres:       record[37],
+	}
+
+	windows := record[17]
+	mac := record[18]
+	linux := record[19]
+
+	AveragePlaytimeForever, err := strconv.Atoi(record[29])
+	if err != nil {
+		return Game{}, fmt.Errorf("Error parsing record: %v", record)
+	}
+	game.AveragePlaytimeForever = AveragePlaytimeForever
+
+	if windows == "True" {
+		game.WindowsCompatible = true
+	}
+	if mac == "True" {
+		game.MacCompatible = true
+	}
+	if linux == "True" {
+		game.LinuxCompatible = true
+	}
+
+	return game, nil
+}
+
+func boolToByte(b bool) byte {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func serializeGame(game *Game) []byte {
+	bytes := make([]byte, 0)
+
+	appIdLen := len(game.AppID)
+	bytes = append(bytes, byte(appIdLen))
+	bytes = append(bytes, []byte(game.AppID)...)
+
+	gameNameLen := len(game.Name)
+	bytes = append(bytes, byte(gameNameLen))
+	bytes = append(bytes, []byte(game.Name)...)
+
+	gameGenresLenBuffer := make([]byte, 8)
+	binary.BigEndian.PutUint64(gameGenresLenBuffer, uint64(len(game.Genres)))
+	bytes = append(bytes, gameGenresLenBuffer...)
+	bytes = append(bytes, []byte(game.Genres)...)
+
+	gameReleaseDateLen := len(game.ReleaseDate)
+	bytes = append(bytes, byte(gameReleaseDateLen))
+	bytes = append(bytes, []byte(game.ReleaseDate)...)
+
+	averagePlaytimeForeverBuffer := make([]byte, 8)
+	binary.BigEndian.PutUint64(averagePlaytimeForeverBuffer, uint64(game.AveragePlaytimeForever))
+	bytes = append(bytes, averagePlaytimeForeverBuffer...)
+
+	bytes = append(bytes, boolToByte(game.WindowsCompatible))
+	bytes = append(bytes, boolToByte(game.MacCompatible))
+	bytes = append(bytes, boolToByte(game.LinuxCompatible))
+
+	return bytes
+}
+
+func deserializeGame(bytes []byte) (Game, error) {
+	index := 0
+
+	appIdLen := uint64(bytes[index])
+	index++
+	appId := string(bytes[index : index+int(appIdLen)])
+	index += int(appIdLen)
+
+	nameLen := uint64(bytes[index])
+	index++
+	name := string(bytes[index : index+int(nameLen)])
+	index += int(nameLen)
+
+	genresLen := binary.BigEndian.Uint64(bytes[index:index+8])
+	index += 8
+	genres := string(bytes[index:(index+int(genresLen))])
+	index += int(genresLen)
+
+	releaseDateLen := uint64(bytes[index])
+	index++
+	releaseDate := string(bytes[index : index+int(releaseDateLen)])
+	index += int(releaseDateLen)
+	
+	averagePlaytimeForever := binary.BigEndian.Uint64(bytes[index : index+8])
+	index += 8
+
+	windowsCompatible := bytes[index] == 1
+	index++
+	macCompatible := bytes[index] == 1
+	index++
+	linuxCompatible := bytes[index] == 1
+	index++
+
+	return Game{
+		AppID:                 appId,
+		Name:                  name,
+		Genres:                genres,
+		ReleaseDate:           releaseDate,
+		AveragePlaytimeForever: int(averagePlaytimeForever),
+		WindowsCompatible:     windowsCompatible,
+		MacCompatible:         macCompatible,
+		LinuxCompatible:       linuxCompatible,
+	}, nil
+}
---
+++ system/communication/protocol/review.go
@@ -0,0 +1,66 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strconv"
+)
+
+type Review struct {
+	AppID       string
+	AppName     string
+	ReviewText  string
+	ReviewScore int8
+	ReviewVotes int
+}
+
+func reviewFromRecord(record []string) (Review, error) {
+	reviewScore, err1 := strconv.Atoi(record[3])
+	reviewVotes, err2 := strconv.Atoi(record[4])
+
+	if err1 != nil || err2 != nil {
+		return Review{}, fmt.Errorf("Error parsing record: %v", record)
+	}
+
+	return Review {
+		AppID:       record[0],
+		AppName:     record[1],
+		ReviewText:  record[2],
+		ReviewScore: int8(reviewScore),
+		ReviewVotes: reviewVotes,
+	}, nil
+}
+
+func serializeReview(review *Review) []byte {
+	bytes := make([]byte, 0)
+
+	appIdLen := len(review.AppID)
+	bytes = append(bytes, byte(appIdLen))
+	bytes = append(bytes, []byte(review.AppID)...)
+
+	bytes = append(bytes, byte(review.ReviewScore))
+
+	reviewTextLenBuffer := make([]byte, 8)
+	binary.BigEndian.PutUint64(reviewTextLenBuffer, uint64(len(review.ReviewText)))
+	bytes = append(bytes, reviewTextLenBuffer...)
+	
+	bytes = append(bytes, []byte(review.ReviewText)...)
+
+	return bytes
+}
+
+func deserializeReview(bytes []byte) (Review, error) {
+	appIdLen := uint64(bytes[0])
+	appId := string(bytes[1:appIdLen+1])
+
+	reviewScore := int8(bytes[appIdLen+1])
+
+	reviewTextLen := binary.BigEndian.Uint64(bytes[appIdLen+2:appIdLen+10])
+	reviewText := string(bytes[appIdLen+10:appIdLen+10+reviewTextLen])
+
+	return Review {
+		AppID:       appId,
+		ReviewScore: reviewScore,
+		ReviewText:  reviewText,
+	}, nil
+}
