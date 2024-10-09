e76c0b1175740126bf699ffed780d44cef9391ef
IgnacioLosc <iloscocco@fi.uba.ar>
2024-10-09 15:23:29 -0300

    Add review mapper

---
+++ system/workers/review_mapper/common/mapper.go
@@ -0,0 +1,165 @@
+package common
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	mw "example.com/system/communication/middleware"
+	"example.com/system/communication/protocol"
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("log")
+
+const (
+	reviews          = "reviews"
+	filtered_reviews = "filtered_reviews"
+)
+
+type ReviewMapperConfig struct {
+	ServerPort string
+}
+
+type ReviewMapper struct {
+	config     ReviewMapperConfig
+	middleware *mw.Middleware
+	stop       chan bool
+}
+
+func NewReviewMapper(config ReviewMapperConfig) (*ReviewMapper, error) {
+	middleware, err := mw.ConnectToMiddleware()
+	if err != nil {
+		return nil, err
+	}
+
+	platformCounter := &ReviewMapper{
+		config:     config,
+		stop:       make(chan bool),
+		middleware: middleware,
+	}
+
+	err = platformCounter.middlewareCounterInit()
+	if err != nil {
+		return nil, err
+	}
+
+	platformCounter.middleware = middleware
+	return platformCounter, nil
+}
+
+func (p ReviewMapper) middlewareCounterInit() error {
+	err := p.middleware.DeclareDirectQueue(reviews)
+	if err != nil {
+		return err
+	}
+
+	err = p.middleware.DeclareExchange(filtered_reviews, "direct")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *ReviewMapper) Close() {
+	p.middleware.Close()
+}
+
+func (p *ReviewMapper) signalListener(cancel context.CancelFunc) {
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt, syscall.SIGTERM)
+	<-chSignal
+	cancel()
+}
+
+func (p *ReviewMapper) Start() {
+	defer p.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go p.signalListener(cancel)
+
+	for {
+		select {
+		case <-ctx.Done():
+			p.stop <- true
+			return
+		default:
+			p.middleware.ConsumeAndProcess(reviews, p.mapReviews)
+		}
+	}
+}
+
+func (p *ReviewMapper) sendEOF() error {
+	err := p.middleware.PublishInExchange(filtered_reviews, "*", []byte("EOF"))
+	if err != nil {
+		log.Info("Error: Couldn't send EOF")
+		return err
+	}
+	return nil
+}
+
+func mapReview(review protocol.Review) protocol.MappedReview {
+	isPositive := review.ReviewScore == 1
+	language := "english"
+	return protocol.MappedReview{AppID: review.AppID, IsPositive: isPositive, IsNegative: !isPositive, IsPositiveEnglish: isPositive && language == "english"}
+}
+
+func (p *ReviewMapper) mapReviews(msg []byte, finished *bool) error {
+	if string(msg) == "EOF" {
+		log.Info("Received EOF. Stopping")
+		*finished = true
+		p.sendEOF()
+		return nil
+	}
+
+	log.Infof("Received review")
+	lenReviews := binary.BigEndian.Uint64(msg[:8])
+
+	log.Infof("Received %d reviews. WITH BUFFER LENGTH: %d", lenReviews, len(msg))
+
+	reviews := make([]protocol.MappedReview, 0)
+
+	index := 8
+	for i := 0; i < int(lenReviews); i++ {
+		review, err, j := protocol.DeserializeReview(msg[index:])
+
+		if err != nil {
+			log.Errorf("Failed to deserialize review: %s", err)
+			continue
+		}
+
+		reviews = append(reviews, mapReview(review))
+		index += j
+	}
+
+	for _, review := range reviews {
+		reviewBuffer := protocol.SerializeMappedReview(&review)
+
+		appId, err := strconv.Atoi(review.AppID)
+		if err != nil {
+			return err
+		}
+		gameRange := appId % 10
+
+		err = p.sendReview(reviewBuffer, gameRange)
+		if err != nil {
+			log.Error("Error sending mapped reviews: %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *ReviewMapper) sendReview(review []byte, gameRange int) error {
+	err := p.middleware.PublishInExchange(filtered_reviews, fmt.Sprintf("%s", gameRange), review)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ system/workers/review_mapper/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"example.com/system/workers/review_mapper/common"
+	"github.com/op/go-logging"
+	"github.com/spf13/viper"
+)
+
+var log = logging.MustGetLogger("log")
+
+func InitConfig() (*viper.Viper, error) {
+	v := viper.New()
+
+	v.AutomaticEnv()
+	v.SetEnvPrefix("cli")
+
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	v.BindEnv("server", "port")
+	v.BindEnv("log", "level")
+
+	return v, nil
+}
+func InitLogger(logLevel string) error {
+	baseBackend := logging.NewLogBackend(os.Stdout, "", 0)
+	format := logging.MustStringFormatter(
+		`%{time:2006-01-02 15:04:05} %{level:.5s}     %{message}`,
+	)
+	backendFormatter := logging.NewBackendFormatter(baseBackend, format)
+
+	backendLeveled := logging.AddModuleLevel(backendFormatter)
+	logLevelCode, err := logging.LogLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	backendLeveled.SetLevel(logLevelCode, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backendLeveled)
+	return nil
+}
+
+func PrintConfig(v *viper.Viper) {
+	log.Infof("action: config | result: sucess | server_port: %s | log_level: %s",
+		v.GetString("server.port"),
+		v.GetString("log.level"),
+	)
+}
+
+func main() {
+	v, err := InitConfig()
+	if err != nil {
+		log.Criticalf("%s", err)
+	}
+
+	if err := InitLogger(v.GetString("log.level")); err != nil {
+		log.Criticalf("%s", err)
+	}
+
+	PrintConfig(v)
+
+	config := common.ReviewMapperConfig{
+		ServerPort: v.GetString("server.port"),
+	}
+	reviewMapper, err := common.NewReviewMapper(config)
+
+	if err != nil {
+		log.Critical("action: bind server | result: fail | err: %s", err)
+	}
+
+	reviewMapper.Start()
+
+}
