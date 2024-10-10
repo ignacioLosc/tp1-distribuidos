767aa27f9fb8e576a44b8c5f834bed92be732971
IgnacioLosc <iloscocco@fi.uba.ar>
2024-10-09 23:47:12 -0300

    Add game review count deserializer in query result

--- system/workers/gateway/common/server.go
+++ system/workers/gateway/common/server.go
@@ -130,6 +130,26 @@ func getGameNames(msg []byte) []string {
 	return games
 }
 
+func getGameReviewCountNames(msg []byte) []string {
+	lenGames := binary.BigEndian.Uint64(msg[:8])
+
+	games := make([]string, 0)
+
+	index := 8
+	for i := 0; i < int(lenGames); i++ {
+		game, err, j := protocol.DeserializeGameReviewCount(msg[index:])
+
+		if err != nil {
+			log.Errorf("Failed to deserialize game", err)
+			continue
+		}
+
+		games = append(games, game.AppName)
+		index += j
+	}
+	return games
+}
+
 func formatGameNames(stringResult string, gameNames []string) {
 	for idx, gameName := range gameNames {
 		if idx > 0 {
@@ -160,19 +180,19 @@ func (s *Server) waitForResults(conn net.Conn) {
 			break
 		case "query3":
 			log.Info("Received results for query 3")
-			gameNames := getGameNames(msg)
+			gameNames := getGameReviewCountNames(msg)
 			stringResult = fmt.Sprintf("QUERY 3 RESULTS: ")
 			formatGameNames(stringResult, gameNames)
 			break
 		case "query4":
 			log.Info("Received results for query 4")
-			gameNames := getGameNames(msg)
+			gameNames := getGameReviewCountNames(msg)
 			stringResult = fmt.Sprintf("QUERY 4 RESULTS: ")
 			formatGameNames(stringResult, gameNames)
 			break
 		case "query5":
 			log.Info("Received results for query 5")
-			gameNames := getGameNames(msg)
+			gameNames := getGameReviewCountNames(msg)
 			stringResult = fmt.Sprintf("QUERY 5 RESULTS: ")
 			formatGameNames(stringResult, gameNames)
 			break
